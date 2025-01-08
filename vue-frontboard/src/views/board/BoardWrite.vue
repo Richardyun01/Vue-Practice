a2fe700b77f44f56b232a2efdb6f9df8442969f7
Richardyun01 <richardyun01@gmail.com>
2025-01-08 10:09:35 +0900

    Added frontboard BoardWrite

--- vue-frontboard/src/views/board/BoardWrite.vue
+++ vue-frontboard/src/views/board/BoardWrite.vue
@@ -1,5 +1,22 @@
 <template>
-
+    <div class="board-detail">
+        <div class="common-buttons">
+            <button type="button" class="w3-button w3-round w3-blue-gray" v-on:click="fnSave">저장</button>&nbsp;
+            <button type="button" class="w3-button w3-round w3-gray" v-on:click="fnList">목록</button>
+        </div>
+        <div class="board-contents">
+            <input type="text" v-model="title" class="w3-input w3-border" placeholder="제목을 입력해주세요.">
+            <input type="text" v-model="author" class="w3-input w3-border" placeholder="작성자를 입력해주세요." v-if="idx === undefined">
+        </div>
+        <div class="board-contents">
+            <textarea id="" cols="30" rows="10" v-model="contents" class="w3-input w3-border" style="resize: none;">
+            </textarea>
+        </div>
+        <div  class="common-buttons">
+            <button type="button" class="w3-button w3-round w3-blue-gray" v-on:click="fnSave">저장</button>&nbsp;
+            <button type="button" class="w3-button w3-round w3-gray" v-on:click="fnList">목록</button>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -20,9 +37,66 @@ export default {
     methods: {
         fnGetView() {
             if (this.idx !== undefined) {
-                
+                this.$axios.get(this.$serverUrl + '/board/' + this.idx, {
+                    params: this.requestBody
+                }).then((res) => {
+                    this.title = res.data.title
+                    this.author = res.data.author
+                    this.contents = res.data.contents
+                    this.created_at = res.data.created_at
+                }).catch((err) => {
+                    console.log(err)
+                })
+            }
+        },
+        fnList() {
+            delete this.requestBody.idx
+            this.$router.push({
+                path: './list',
+                query: this.requestBody
+            })
+        },
+        fnView(idx) {
+            this.requestBody.idx = idx
+            this.$router.push({
+                path: './detail',
+                query: this.requestBody
+            })
+        },
+        fnSave() {
+            let apiUrl = this.$serverUrl + '/board'
+            this.form = {
+                "idx": this.idx,
+                "title": this.title,
+                "contents": this.contents,
+                "author": this.author,
+            }
+
+            if (this.idx === undefined) {
+                this.$axios.post(apiUrl, this.form)
+                    .then((res) => {
+                        alert('글이 저장되었습니다.')
+                        this.fnView(res.data.idx)
+                }).catch((err) => {
+                    if (err.message.indexOf('Network Error') > -1) {
+                        alert('서버와 통신이 불안정합니다.')
+                    }
+                })
+            } else {
+                this.$axios.patch(apiUrl, this.form)
+                    .then((res) => {
+                        alert('글이 저장되었습니다.')
+                        this.fnView(res.data.idx)
+                }).catch((err) => {
+                    if (err.message.indexOf('Network Error') > -1) {
+                        alert('서버와 통신이 불안정합니다.')
+                    }
+                })
             }
         }
     }
 }
 </script>
+<style scoped>
+
+</style>
